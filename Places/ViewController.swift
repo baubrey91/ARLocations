6b8e44f3b578a637739762beb585f8ec03675809
Brandon Aubrey <brandonaubrey@brandons-mbp-2>
2017-04-23 23:13:56 -0700

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,126 @@
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var mapView: MKMapView!
+  fileprivate let locationManager = CLLocationManager()
+  fileprivate var startedLoadingPOIs = false
+  fileprivate var places = [Place]()
+  
+  fileprivate var arViewController: ARViewController!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    locationManager.delegate = self
+    locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+    locationManager.startUpdatingLocation()
+    locationManager.requestWhenInUseAuthorization()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func showARController(_ sender: Any) {
+    arViewController = ARViewController()
+    
+    arViewController.dataSource = self
+    
+    arViewController.maxVisibleAnnotations = 30
+    arViewController.headingSmoothingFactor = 0.05
+    
+    arViewController.setAnnotations(places)
+    
+    self.present(arViewController, animated: true, completion: nil)
+  }
+  
+  func showInfoView(forPlace place: Place) {
+    let alert = UIAlertController(title: place.placeName , message: place.infoText, preferredStyle: UIAlertControllerStyle.alert)
+    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+    
+    arViewController.present(alert, animated: true, completion: nil)
+  }
+}
+
+extension ViewController: CLLocationManagerDelegate{
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    
+    if locations.count > 0 {
+      let location = locations.last!
+      print("Accuracy: \(location.horizontalAccuracy)")
+      
+      if location.horizontalAccuracy < 100 {
+        manager.stopUpdatingLocation()
+        let span = MKCoordinateSpan(latitudeDelta: 0.014, longitudeDelta: 0.014)
+        let region = MKCoordinateRegion(center: location.coordinate, span: span)
+        mapView.region = region
+        
+        if !startedLoadingPOIs {
+          startedLoadingPOIs = true
+          
+          let loader = PlacesLoader()
+          loader.loadPOIS(location: location, radius: 1000) { placesDict, error in
+            if let dict = placesDict {
+              guard let placesArray = dict.object(forKey: "results") as? [NSDictionary] else { return }
+              
+              for placeDict in placesArray {
+                let latitude = placeDict.value(forKeyPath: "geometry.location.lat") as! CLLocationDegrees
+                let longitude = placeDict.value(forKeyPath: "geometry.location.lng") as! CLLocationDegrees
+                let reference = placeDict.value(forKey: "reference") as! String
+                let name = placeDict.value(forKey: "name") as! String
+                let address = placeDict.value(forKey: "vicinity") as! String
+                
+                let location = CLLocation(latitude: latitude, longitude: longitude)
+
+                let place = Place(location: location, reference: reference, name: name, address: address)
+                
+                self.places.append(place)
+
+                let annotation = PlaceAnnotation(location: place.location!.coordinate, title: place.placeName)
+                
+                DispatchQueue.main.sync {
+                  self.mapView.addAnnotation(annotation)
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+extension ViewController: ARDataSource {
+  func ar(_ arViewController: ARViewController, viewForAnnotation: ARAnnotation) -> ARAnnotationView {
+    let annotationView = AnnotationView()
+    annotationView.annotation = viewForAnnotation
+    annotationView.delegate = self
+    annotationView.frame = CGRect(x: 0, y: 0, width: 150, height: 50)
+    
+    return annotationView
+  }
+}
+
+extension ViewController: AnnotationViewDelegate {
+  func didTouch(annotationView: AnnotationView) {
+    
+    if let annotation = annotationView.annotation as? Place {
+      let placesLoader = PlacesLoader()
+      placesLoader.loadDetailInformation(forPlace: annotation) { resultDict, error in
+        
+        if let infoDict = resultDict?.object(forKey: "result") as? NSDictionary {
+          annotation.phoneNumber = infoDict.object(forKey: "formatted_phone_number") as? String
+          annotation.website = infoDict.object(forKey: "website") as? String
+          
+          self.showInfoView(forPlace: annotation)
+        }
+      }
+    }
+  }
+}
